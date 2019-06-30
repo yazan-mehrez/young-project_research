52974d9351f9effc4fa6dcf67686687a58720757
yazan <yazan.mehrez@gmail.com>
2019-06-30 10:14:39 +0300

    slider

--- src/app/civil/home/home.component.ts
+++ src/app/civil/home/home.component.ts
@@ -1,25 +1,44 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {AppService} from '../../app.service';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {fadeInOut} from '../../../animations/fadeInOut';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
-    animations: [
-        trigger('fadeInOut', [
-            state('void', style({
-                opacity: 0
-            })),
-            transition('void => *', animate(1000)),
-        ]),
-    ]
+    animations: [fadeInOut]
 })
 export class HomeComponent implements OnInit {
     collapsed = false;
+    itemIndex = 0;
+    dataList = [
+        {
+            photo: 'assets/img/library.jpg',
+            text: 'Filler text is text that shares some characteristics of a real written text, but is random or otherwise generated. It may be used to display a sample of fonts, generate text for testing, or to spoof an e-mail spam filter.'
+        },
+        {
+            photo: 'assets/img/img223.jpg',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad asperiores atque cumque dignissimos doloremque ducimus eligendi excepturi iusto, minus nemo neque nostrum quae, qui quis quod ratione rem tenetur.'
+        }];
 
     constructor(private translateService: TranslateService, private _appService: AppService) {
+    }
+
+    next() {
+        if (this.itemIndex == (this.dataList.length - 1)) {
+            this.itemIndex = 0;
+        } else {
+            this.itemIndex += 1;
+        }
+    }
+
+    prev() {
+        if (this.itemIndex == 0) {
+            this.itemIndex = (this.dataList.length - 1);
+        } else {
+            this.itemIndex -= 1;
+        }
     }
 
     ngOnInit() {
